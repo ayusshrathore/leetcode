ec772003e63125bb30742f4e8cc881ae13888a8b
Aarav Mishra <aaravmishra619@gmail.com>
2023-04-16 19:34:44 +0530

    Time: 391 ms (8.53%), Space: 45.1 MB (67.07%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,35 +1,24 @@
-class Solution {
-    private int n;
-    
+class Solution { // bottom up
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        this.n = piles.size();
+        int n = piles.size();
         
-        int[][] dp = new int[n+1][k+1];
+        int[][] dp = new int[n+1][k+1]; // dp[n][coins] states that upto pile n we can take max 'coins' coins
         
-        for(int[] arr : dp)
-            Arrays.fill(arr, -1);
-        
-        return solve(0, k, piles, dp);
-    }
-    public int solve(int i, int k, List<List<Integer>> piles, int[][] dp) {
-        if(i >= n)
-            return 0;
-        
-        if(dp[i][k] != -1)
-            return dp[i][k];
-        
-        int notTaken = solve(i+1, k, piles, dp); // idx goes on, but k remains the same
-        
-        int taken = Integer.MIN_VALUE, sum = 0;
-        
-            for(int j = 0; j < Math.min(k, piles.get(i).size()); j++) { // min(number of coins left to be collected, size of pile)
-                sum += piles.get(i).get(j);
+        for(int i = 1; i <= n; i++) // starting from 1st pile filling the rows
+            
+            for(int coins = 1; coins <= k; coins++) { // going from zero upto k coins in each pile (filling the cols)
                 
-                if(k-(j+1) >= 0)
-                    taken = Math.max(taken, sum + solve(i+1, k-(j+1), piles, dp)); // calculate the the sum of coins for the next pile
+                int sum = 0;
                 
-            }
-        
-        return dp[i][k] = Math.max(taken, notTaken);
+                for(int currCoins = 0; currCoins <= Math.min(piles.get(i-1).size(), coins); currCoins++) { // currCoins -> no. of coins taken so far
+                    
+                    if(currCoins > 0)
+                        sum += piles.get(i-1).get(currCoins-1);
+                    
+                    dp[i][coins] = Math.max(dp[i][coins], sum + dp[i-1][coins-currCoins]); 
+                }
+            }   
+                
+        return dp[n][k];
     }
 }

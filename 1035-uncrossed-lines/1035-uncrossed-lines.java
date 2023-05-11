c71f0802030ce8793b2d31e8281475d911602fb5
Aarav Mishra <aaravmishra619@gmail.com>
2023-05-11 22:30:49 +0530

    Time: 5 ms (92.56%), Space: 41.9 MB (90.82%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,4 +1,4 @@
-class Solution { // top-down approach
+class Solution { // bottom-down approach
         /*
          i  
         [1, 4, 2] // case if both the elements at each indices are same
@@ -19,21 +19,16 @@ class Solution { // top-down approach
         int n = nums1.length, m = nums2.length;
         int[][] dp = new int[n+1][m+1];
         
-        for(int[] arr : dp) // 2 indices are changing i, j
-            Arrays.fill(arr, -1);
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++) {
+                if(nums1[i-1] == nums2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+            
+            else 
+                dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+            
+        }
         
-        return solve(0, 0, n, m, nums1, nums2, dp);
-    }
-    public int solve(int i, int j, int n, int m, int[] nums1, int[] nums2, int[][] dp) {
-        if(i >= n || j >= m) // as soon as one of the index goes out of bounds
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        if(nums1[i] == nums2[j])
-            return dp[i][j] = 1 + solve(i+1, j+1, n, m, nums1, nums2, dp);
-        
-        return dp[i][j] = Math.max(solve(i, j+1, n, m, nums1, nums2, dp), solve(i+1, j, n, m, nums1, nums2, dp)); // otherwise max of both (either i+1, j+1) the possibilities
+        return dp[n][m];
     }
 }

2556b4756b5cca08ce16e2168e646d57eebde705
Aarav Mishra <aaravmishra619@gmail.com>
2023-08-08 22:44:38 +0530

    Time: 5 ms (96.42%), Space: 49.2 MB (50.06%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,27 +1,21 @@
-class Solution { // binary search O(4^(log₂(min(n, m))))
+class Solution { // linear search O(n+m)
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
-        
-        return binarySearch(matrix, target, 0, 0, n - 1, m - 1);
-    }
-    
-    private boolean binarySearch(int[][] matrix, int target, int rowStart, int colStart, int rowEnd, int colEnd) {
-        if (rowStart > rowEnd || colStart > colEnd) {
-            return false;
+
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++; // since curr element is smaller than target and rows are sorted in ascending
+            } else {
+                col--;
+            }
         }
-        
-        int midRow = (rowStart + rowEnd) / 2;
-        int midCol = (colStart + colEnd) / 2;
-        
-        if (matrix[midRow][midCol] == target) {
-            return true;
-        } else if (matrix[midRow][midCol] < target) {
-            return binarySearch(matrix, target, rowStart, midCol + 1, midRow, colEnd) || 
-                   binarySearch(matrix, target, midRow + 1, colStart, rowEnd, colEnd);
-        } else {
-            return binarySearch(matrix, target, rowStart, colStart, midRow - 1, colEnd) || 
-                   binarySearch(matrix, target, midRow, colStart, rowEnd, midCol - 1);
-        }
+
+        return false;
     }
 }

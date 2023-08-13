578e8b364123e047e4514fca22d04ee0cf145c37
Aarav Mishra <aaravmishra619@gmail.com>
2023-08-13 12:17:11 +0530

    Time: 13 ms (34.95%), Space: 62.9 MB (18.45%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -19,27 +19,34 @@ class Solution { // DP, TOP-DOWN, TC: O(n)
         if(i+1 < n && nums[i] == nums[i+1]) {
             result |= solve(i+2, n, nums, dp);
 <<<<<<< HEAD
-=======
-            
-            if(result)
-                return dp[i] = true;
->>>>>>> edca53a (Time: 10 ms (61.17%), Space: 63.1 MB (15.53%) - LeetHub)
-        }
-        
-        // rule 2
-        if(i+2 < n && nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) {
-            result |= solve(i+3, n, nums, dp);
 <<<<<<< HEAD
 =======
             
             if(result)
                 return dp[i] = true;
 >>>>>>> edca53a (Time: 10 ms (61.17%), Space: 63.1 MB (15.53%) - LeetHub)
+=======
+>>>>>>> b86bd33 (Time: 13 ms (34.95%), Space: 62.9 MB (18.45%) - LeetHub)
+        }
+        
+        // rule 2
+        if(i+2 < n && nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) {
+            result |= solve(i+3, n, nums, dp);
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+            
+            if(result)
+                return dp[i] = true;
+>>>>>>> edca53a (Time: 10 ms (61.17%), Space: 63.1 MB (15.53%) - LeetHub)
+=======
+>>>>>>> b86bd33 (Time: 13 ms (34.95%), Space: 62.9 MB (18.45%) - LeetHub)
         }
         
         // rule 3
         if(i+2 < n && nums[i+1] - nums[i] == 1 && nums[i+2] - nums[i+1] == 1) {
             result |= solve(i+3, n, nums, dp);
+<<<<<<< HEAD
 <<<<<<< HEAD
         }
         
@@ -52,5 +59,10 @@ class Solution { // DP, TOP-DOWN, TC: O(n)
         
         return dp[i] = false;
 >>>>>>> edca53a (Time: 10 ms (61.17%), Space: 63.1 MB (15.53%) - LeetHub)
+=======
+        }
+        
+        return dp[i] = result;
+>>>>>>> b86bd33 (Time: 13 ms (34.95%), Space: 62.9 MB (18.45%) - LeetHub)
     }
 }
